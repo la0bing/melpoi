e9c95081329348854cf388941024a2414ea07ce9
Melvin Low <la0bing07148@gmail.com>
2022-09-28 11:49:34 +0800

    fix: pre commit fix

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,10 +6,10 @@ module.exports = {
     //    issuePrefixes: ['[A-Z]{1,8}-[0-9]{1,4}'],
     //    referenceActions: null,
     //  },
-    //},
+    // },
     rules: {
       'footer-max-line-length': [0],
-      'references-empty': [2, 'never'],
+      // 'references-empty': [2, 'never'],
       'subject-case': [0],
       'type-case': [2, 'always', 'lower-case'],
     },
